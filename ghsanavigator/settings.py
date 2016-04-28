bed7b7de69e4f68401072ea692074ff59708df8b
Ellen <ellenltsay@gmail.com>
2016-04-27 18:09:29 -0700

    switch to sqlite

--- ghsanavigator/settings.py
+++ ghsanavigator/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'ghsanavigator.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'djangogirls',
+#        'USER': 'ellen',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'ellen',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
