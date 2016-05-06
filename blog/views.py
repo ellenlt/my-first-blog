e983d8e37c02ca3ed69bbda4ca1bb0a4deddc188
Ellen <ellenltsay@gmail.com>
2016-05-06 09:37:00 -0700

    Displaying posts on page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+# Import model from the models file in the current directory
+from .models import Post
+# Because we use timezone in this file
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
+	# Create a variable (aka name) for our QuerySet
+	# lte = less than or equal to
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	# Render function will take a request and render the template
-	# Template name corresponds to the URL, 
-	# so URL "blog/post_list.html" -> file in blog/template/blog/post_list.html
-	return render(request, 'blog/post_list.html', {})
+	# Template name corresponds to the URL, so URL "blog/post_list.html" -> 
+	# file in blog/template/blog/post_list.html
+	# We also pass along the posts parameter to the view
+	return render(request, 'blog/post_list.html', {'posts': posts})
