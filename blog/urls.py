15f10bdfc4d7cb0a61c10cedb89365e704da7572
Ellen <ellenltsay@gmail.com>
2016-05-06 22:37:58 -0700

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+# Stores information from url and assigns specific urls to views
 urlpatterns = [
 	# Assigning a view called post_list to an empty URL
 	# The name of the URL is separate from the view name
 	url(r'^$', views.post_list, name='post_list'),
+	# The regexp ^post/(?P<pk>\d+)$ means that Django will take everything
+	# after post/? and transfer to a view as a new variable called pk (primary key)
+	# And \d tells us that it can only be a digit (so ignores the last backslash)
+	url(r'^post/(?P<pk>\d+)$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Import model from the models file in the current directory
 from .models import Post
 # Because we use timezone in this file
 from django.utils import timezone
 
+
 # Create your views here.
+# Tells Django what variables to pass to the view
+# and renders the view
 def post_list(request):
 	# Create a variable (aka name) for our QuerySet
 	# lte = less than or equal to
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	# Render function will take a request and render the template
-	# Template name corresponds to the URL, so URL "blog/post_list.html" -> 
-	# file in blog/template/blog/post_list.html
+	# Render function will take a request to display a view (post_list)
+	# and render the corresponding template (post_list.html).
+	# Usually template names match view names
 	# We also pass along the posts parameter to the view
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	# get_object_or_404 is a built in Django function that handles the case where
+	# no pk was passed in
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+
